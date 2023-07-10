79bf4c66de59a0a0f709ce16960bb5fc8f836bcf
ErinKace <erinkacerovskis@gmail.com>
2023-07-10 10:06:27 -0500

    Opps I forgot to set up a respository

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,44 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    //handles request of the form hello?name=LaunchCode
+    @RequestMapping(value="hello", method={RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
+    public String helloWithQuery(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+    // handles requests of the form hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" +
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+}
